eec3218242b5061c7b9872bd7952eef648f934cf
Vlad Kuzmin <devlad@bk.ru>
2020-03-30 16:05:06 +0200

    ch21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,30 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
         return (
-            <div>
-                Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Vlad
+                    </div>
+                    <div className={s.dialog}>
+                        Max Payne
+                    </div>
+                    <div className={s.dialog}>
+                        Ronaldinho
+                    </div>
+                    <div className={s.dialog}>
+                        Darth Vader
+                    </div>
+                    <div className={s.dialog}>
+                        Mr. Putin
+                    </div>
+                    </div>                
+            
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>What's up nigga?</div>
+                <div className={s.dialog}>Hey nigga!</div>
+            </div>
             </div>
         )
 }
