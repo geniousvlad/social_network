d8e19fc18f537e78bf55b7434ccc1810f8f27efc
Vlad Kuzmin <devlad@bk.ru>
2020-04-28 16:49:00 +0200

    ch63

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Users.module.css';
 import noUser from '../../assets/images/no_user.png';
 import { NavLink } from 'react-router-dom';
+import * as axios from 'axios';
 
 
 let Users = (props) => {
@@ -12,6 +13,8 @@ let Users = (props) => {
         for (let i=1; i <= pagesCount; i++) {
             pages.push(i);
         }
+
+    
 
     return <div>
     <div>
@@ -30,8 +33,39 @@ let Users = (props) => {
             </div>
             <div>
     { u.followed ? 
-        <button onClick={ () => { props.unfollow(u.id) } } >Unfollow</button> : 
-        <button onClick={ () => { props.follow(u.id) } }>Follow</button> }
+        <button onClick={ () => {
+
+            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${ u.id }`, {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '7437669a-efca-4450-b484-2090cbbc8809'
+                    }
+                })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    props.unfollow(u.id);
+                }
+        });
+
+
+             } } >Unfollow</button>
+              : <button onClick={ () => {
+
+                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${ u.id }`, {}, {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '7437669a-efca-4450-b484-2090cbbc8809'
+                    }
+
+                })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    props.follow(u.id);
+                }
+        });
+
+
+              } }>Follow</button> }
             </div>
         </span>
         <span>
