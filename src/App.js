b74c839d854afd9c11ef03776237e0e4551f19a5
Vlad Kuzmin <devlad@bk.ru>
2020-04-28 13:03:08 +0200

    ch62

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -10,12 +8,13 @@ import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
   
   return (
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
           <Route path='/news' component={News} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTv-58tqxqlkRCHf59OmRzM6z03uSN-3qTeb6Yx6xnyYQzGxvMe" />
+
+        <div className={ s.loginBlock }>
+          { props.isAuth ? props.login
+              : <NavLink to={ '/login' }>Login</NavLink> }
+        </div>
       </header>
     )
 }
