0e31746cf97e217fe8719626e3fa43c15eec54c6
Vlad Kuzmin <devlad@bk.ru>
2020-05-12 09:47:35 +0200

    ch68

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
 const App = () => {
   
@@ -28,7 +29,10 @@ const App = () => {
                  render={ () => <ProfileContainer /> } />
 
           <Route path='/users' 
-                 render={ () => <UsersContainer /> } />                                         
+                 render={ () => <UsersContainer /> } />
+
+          <Route path='/login' 
+                 render={ () => <Login /> } />                                         
 
         </div>
       </div>
