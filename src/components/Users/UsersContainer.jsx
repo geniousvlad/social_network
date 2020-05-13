30d2de14466900fb259dbe0b62532c16734b8486
Vlad Kuzmin <devlad@bk.ru>
2020-05-13 12:00:52 +0200

    ch70

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -5,6 +5,7 @@ import Users from './Users';
 import Preloader from '../Common/Preloader/Preloader';
 import { usersAPI } from '../../api/api';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 
 class UsersContainer extends React.Component {
@@ -47,9 +48,12 @@ let mapStateToProps = (state) => {
     }
 }
 
-export default withAuthRedirect(connect(mapStateToProps, {
-    follow,
-    unfollow,
-    setCurrentPage,        
-    toggleFollowingProgress, getUsers
-    }) (UsersContainer));
+export default compose (
+    withAuthRedirect,
+    connect(mapStateToProps, {
+        follow,
+        unfollow,
+        setCurrentPage,        
+        toggleFollowingProgress, getUsers
+        })
+) (UsersContainer);
