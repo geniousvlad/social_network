7a640541de560489ad9e88b391a158e74fe48527
Vlad Kuzmin <devlad@bk.ru>
2020-06-04 16:40:59 +0200

    ch78

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,8 @@ const Header = (props) => {
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTv-58tqxqlkRCHf59OmRzM6z03uSN-3qTeb6Yx6xnyYQzGxvMe" />
 
         <div className={ s.loginBlock }>
-          { props.isAuth ? props.login
+          { props.isAuth 
+          ? <div> { props.login } - <button onClick={ props.logout }>Log out</button> </div>
               : <NavLink to={ '/login' }>Login</NavLink> }
         </div>
       </header>
