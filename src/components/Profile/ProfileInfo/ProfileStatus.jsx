64574bdeb9308a523dcea1d2934b82939c8f05dd
Vlad Kuzmin <devlad@bk.ru>
2020-06-09 15:15:03 +0200

    ch84

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -50,7 +50,10 @@ class ProfileStatus extends React.Component {
                 }
                 {this.state.editMode &&
                     <div>
-                        <input onChange={ this.onStatusChange } autoFocus={ true } onBlur={ this.deactivateEditMode } value={this.state.status} />
+                        <input onChange={ this.onStatusChange } 
+                               autoFocus={ true } 
+                               onBlur={ this.deactivateEditMode } 
+                               value={this.state.status} />
                     </div>
                 }
             </div>
