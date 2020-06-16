0d888cd7dc5022ef07a3278b774bf116b3aad70e
Vlad Kuzmin <devlad@bk.ru>
2020-06-16 16:49:12 +0200

    ch96

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../Common/Preloader/Preloader';
-import ProfileStatus from './ProfileStatus';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import noUser from '../../../assets/images/no_user.png';
 
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
 
   if (!profile) {
     return <Preloader />
   }
 
+    const onMainPhotoSelected = (e) => {
+      if (e.target.files.length) {
+        savePhoto(e.target.files[0]);
+      }      
+    }
+ 
     return (
         <div>
         <div className={s.descriptionBlock}>
-          <img src={ profile.photos.large } />
+          <img src={ profile.photos.large || noUser} className={ s.mainPhoto } />
+            { isOwner && <input type={ 'file' } onChange={ onMainPhotoSelected }/> }
           <ProfileStatusWithHooks status={ status } updateStatus={ updateStatus } />
         </div>
       </div>
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import { usersAPI, profileAPI } from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
 const SET_STATUS = 'SET-STATUS';
+const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 let initialState = {
     posts: [
@@ -36,6 +37,10 @@ const profileReducer = (state = initialState, action) => {
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
+
+        case SAVE_PHOTO_SUCCESS: {
+            return {...state, profile: {...state.profile, photos: action.photos}}
+        }
         default:
             return state;
 
@@ -46,6 +51,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostText })    
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })  
 export const setStatus = (status) => ({ type: SET_STATUS, status })  
+export const savePhotoSuccess = (photos) => ({ type: SAVE_PHOTO_SUCCESS, photos })  
 
 export const getUserProfile = (userId) => async (dispatch) => {
     const response = await usersAPI.getProfile (userId);
@@ -61,6 +67,14 @@ export const updateStatus = (status) => async (dispatch) => {
     let response = await profileAPI.updateStatus (status);
         if ( response.data.resultCode === 0 ) {
         dispatch(setStatus(status));
+        }
+}
+
+export const savePhoto = (file) => async (dispatch) => {
+    let response = await profileAPI.savePhoto(file);
+
+        if ( response.data.resultCode === 0 ) {
+        dispatch(savePhotoSuccess(response.data.data.photos));
         }
 }
 
